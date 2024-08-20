1f99b39b27c47eb0f5b6358b29864a4180b898cc
Spencer <spencer.adler2@gmail.com>
2024-08-20 18:28:13 -0400

    Updated for render deployment

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from config import app
-
+from flask import render_template
 from models.user import User
 from models.location import Location
 from models.travel_plan import TravelPlan
@@ -7,6 +7,10 @@ from models.travel_plan import TravelPlan
 from routes.TravelPlans import *
 from routes.Users import *
 from routes.Locations import *
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
